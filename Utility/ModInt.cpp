d825e98f8f5874b848f4b7afa37f27b5c635a832
yoavs-shamay <yoavsh97@gmail.com>
2023-11-15 16:07:17 +0200

    Fixed an error in ModInt

--- Utility/ModInt.cpp
+++ Utility/ModInt.cpp
@@ -13,10 +13,10 @@ typedef struct ModInt
         return res;
     }
     ModInt operator/(const ModInt &other) const { return ModInt(val * other.pow(mod - 2).val); }
-    ModInt operator+=(const ModInt &other) { return *this = *this + other; }
-    ModInt operator-=(const ModInt &other) { return *this = *this - other; }
-    ModInt operator*=(const ModInt &other) { return *this = *this * other; }
-    ModInt operator/=(const ModInt &other) { return *this = *this / other; }
+    ModInt &operator+=(const ModInt &other) { return *this = *this + other; }
+    ModInt &operator-=(const ModInt &other) { return *this = *this - other; }
+    ModInt &operator*=(const ModInt &other) { return *this = *this * other; }
+    ModInt &operator/=(const ModInt &other) { return *this = *this / other; }
     bool operator==(const ModInt &other) const { return val == other.val; }
     bool operator!=(const ModInt &other) const { return val != other.val; }
     friend ostream &operator<<(ostream &os, const ModInt &other) { return os << other.val; }
