c884c15a4dd806386473a263bd2e9f1a026a8f9d
yoavs-shamay <yoavsh97@gmail.com>
2024-05-15 08:35:45 +0300

    changes

--- Algorithms/BinaryLifting.cpp
+++ Algorithms/BinaryLifting.cpp
@@ -6,7 +6,11 @@ vv toLifting(v &next)
     for (ll i = 0; i < N; i++) res[i][0] = next[i];
     for (ll iter = 1; iter <= logN; iter++)
     {
-        for (ll i= 0 ; i < N; i++) res[i][iter] = res[res[i][iter - 1]][iter - 1];
+        for (ll i= 0 ; i < N; i++)
+        {
+            if (0 > res[i][iter - 1] || res[i][iter - 1] >= N) res[i][iter] = res[i][iter - 1];
+            else res[i][iter] = res[res[i][iter - 1]][iter - 1];
+        }
     }
     return res;
 }
