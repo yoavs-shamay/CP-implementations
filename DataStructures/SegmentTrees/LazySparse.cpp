e5588ab23fa264fe79c8ace882d75fcd7c40703d
yoavs-shamay <yoavsh97@gmail.com>
2024-05-18 15:17:01 +0300

    Added lazy sparse segtree

--- DataStructures/SegmentTrees/LazySparse.cpp
+++ DataStructures/SegmentTrees/LazySparse.cpp
@@ -1,1 +1,75 @@
-//TODO implement
+struct SegmentTree
+{
+    ll size;
+    operation op;
+    ll defaultValue;
+    ll lazyDefault;
+    operation lazyOp;
+    struct Node
+    {
+        Node *left;
+        Node *right;
+        ll value;
+        ll lazy;
+        Node(ll val, ll lazyVal) : value(val), lazy(lazyVal), left(NULL), right(NULL) {}
+        Node *getLeft(ll def, ll lazyDef)
+        {
+            if (!left) left = new Node(def, lazyDef);
+            return left;
+        }
+        Node *getRight(ll def, ll lazyDef)
+        {
+            if (!right) right = new Node(def, lazyDef);
+            return right;
+        }
+    };
+    Node *root;
+    SegmentTree(operation op, ll defaultValue, operation lazyOp, ll lazyDefault, ll len)
+    {
+        size = (1ll << (64 - __builtin_clzll(len - 1)));
+        this->op = op;
+        this->defaultValue = defaultValue;
+        this->lazyOp = lazyOp;
+        this->lazyDefault = lazyDefault;
+        root = new Node(defaultValue, lazyDefault);
+    }
+    void _push(Node *cur)
+    {
+        Node *l = cur->getLeft(defaultValue, lazyDefault), *r = cur->getRight(defaultValue, lazyDefault);
+        l->lazy = lazyOp(l->lazy, cur->lazy);
+        r->lazy = lazyOp(r->lazy, cur->lazy);
+        cur->lazy = lazyDefault;
+    }
+    ll _query(Node *cur, ll l, ll r, ll tl, ll tr)
+    {
+        if (tl >= l && tr <= r)
+            return cur->value;
+        if (tr < l || tl > r) return defaultValue;
+        _push(cur);
+        ll tmid = (tl + tr) / 2;
+        return op(_query(cur->getLeft(defaultValue, lazyDefault), l, min(r, tmid), tl, tmid), _query(cur->getRight(defaultValue, lazyDefault), max(l, tmid + 1), r, tmid + 1, tr));
+    }
+    ll query(ll l, ll r)
+    {
+        return _query(root, l, r, 0, size - 1);
+    }
+    void _update(Node *cur, ll from, ll to, ll updateBy, ll tl, ll tr)
+    {
+        _push(cur);
+        if (tl >= from && tr <= to)
+        {
+            cur->value = lazyOp(cur->value, updateBy);
+            cur->lazy = lazyOp(cur->value, updateBy);
+            return;
+        }
+        if (to < tl || from > tr) return;
+        ll tmid = (tl + tr) / 2;
+        _update(cur->getLeft(defaultValue, lazyDefault), from, min(to, tmid), newVal, tl, tmid);
+        _update(cur->getRight(defaultValue, lazyDefault), max(from, tmid + 1), to, newVal, tmid + 1, tr);
+        cur->value = op(cur->getLeft(defaultValue, lazyDefault)->value, cur->getRight(defaultValue, lazyDefault)->value);
+    }
+    void update(ll from, ll to, ll updateBy)
+    {
+        _update(root, from, to, updateBy, 0, size - 1);
+    }
+};
